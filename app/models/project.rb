bd1f35bd38d290f3df82bd736ca628211b20ddda
Rob Warner <rwarner@grailbox.com>
2011-05-23 16:06:04 -0400

    Fix bug: count only weeks in displayed range, not all weeks

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,13 @@ class Project < ActiveRecord::Base
   has_many :project_weeks
   has_many :weeks, :through => :project_weeks, :uniq => true
 
+  def num_weeks_scheduled weeks
+    num = 0
+    weeks.each do |week|
+      if self.weeks.include?(week)
+        num += 1
+      end
+    end
+    num
+  end
 end
