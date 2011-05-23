2be15ae3b03b005c8e94451cf58ddfeeb9c70fae
Rob Warner <rwarner@grailbox.com>
2011-05-23 09:23:54 -0400

    Add ability to edit projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -26,6 +26,20 @@ class ProjectsController < ApplicationController
     end
   end
 
+  def edit
+    @project = Project.find(params[:id])
+    do_weeks
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(params[:project])
+      redirect_to(@project)
+    else
+      render :action => "edit"
+    end
+  end
+
   def destroy
     @project = Project.find(params[:id])
     @project.destroy
